f0533889a16e88cb6a3023ce3547ce4e6e5a35a2
JJput <281138580@qq.com>
2021-02-25 15:39:34 +0800

    移除email验证码和手机验证码缓存工具，将手机号验证码加入RedisManage中

--- server/src/main/java/com/twj/spirngbasics/server/manage/RedisManage.java
+++ server/src/main/java/com/twj/spirngbasics/server/manage/RedisManage.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.twj.spirngbasics.server.entity.User;
 import com.twj.spirngbasics.server.util.RedisUtils;
 
+import javax.validation.constraints.NotNull;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,9 +16,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class RedisManage {
 
-    //用户信息缓存    30天后过期
-    private static final int USER_INFO_TIME = 30;
+    //用户信息缓存  3天后过期
+    private static final int USER_INFO_TIME = 3;
     private static final TimeUnit USER_INFO_TIME_UNIT = TimeUnit.DAYS;
+
+    //手机号验证码  5分钟后过期
+    private static final int PHONE_CODE_TIME = 5;
+    private static final TimeUnit PHONE_CODE_TIME_UNIT = TimeUnit.MINUTES;
 
     /**
      * 设置用户token
@@ -25,16 +30,17 @@ public class RedisManage {
      * @param k 键
      * @param v 值
      */
-    public static void setUserToken(String k, User v) {
+    public static void setUserToken(@NotNull String k, @NotNull User v) {
         RedisUtils.set(k, JSONObject.toJSONString(v), USER_INFO_TIME, USER_INFO_TIME_UNIT);
     }
 
     /**
      * 获取用户token
+     *
      * @param k 键
      * @return 一般为json格式
      */
-    public static String getUserToken(String k) {
+    public static String getUserToken(@NotNull String k) {
         if (k == null)
             return null;
         Object object = RedisUtils.get(k);
@@ -44,10 +50,34 @@ public class RedisManage {
     }
 
     /**
+     * 设置手机号验证码
+     *
+     * @param phone 手机号
+     * @param code  验证码
+     */
+    public static void setPhoneCode(@NotNull String phone, @NotNull String code) {
+        RedisUtils.set(phone, code, PHONE_CODE_TIME, PHONE_CODE_TIME_UNIT);
+    }
+
+    /**
+     * 获取手机号验证码
+     *
+     * @param phone 手机号
+     */
+    public static String getPhoneCode(@NotNull String phone) {
+        Object object = RedisUtils.get(phone);
+        if (object == null)
+            return null;
+        return object.toString();
+    }
+
+
+    /**
      * 移除
+     *
      * @param k 键
      */
-    public static void remove(String k) {
+    public static void remove(@NotNull String k) {
         RedisUtils.del(k);
     }
 
