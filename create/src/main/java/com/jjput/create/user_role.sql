4426c8c64475daaea34840bea324e433e89c72e3
JJput <281138580@qq.com>
2021-02-26 13:39:16 +0800

    修改用户sql语句(主要修改字段顺序)

--- create/src/main/java/com/jjput/create/user_role.sql
+++ create/src/main/java/com/jjput/create/user_role.sql
@@ -24,6 +24,7 @@ FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `sys_dict`;
 CREATE TABLE `sys_dict`
 (
+    `id`           varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '编号',
     `created_by`   varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '创建人',
     `created_time` datetime                                               NOT NULL COMMENT '创建时间',
     `update_by`    varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '更新人',
@@ -31,7 +32,6 @@ CREATE TABLE `sys_dict`
     `dele`         varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin   NOT NULL DEFAULT '1' COMMENT '删除',
     `remake`       varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin          DEFAULT NULL COMMENT '备注',
     `spare1`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '备用1',
-    `id`           varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '编号',
     `value`        varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '数据值',
     `label`        varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '标签名',
     `type`         varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '类型',
@@ -80,15 +80,15 @@ DROP TABLE IF EXISTS `user_resource`;
 CREATE TABLE `user_resource`
 (
     `id`           varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL DEFAULT '' COMMENT 'id',
-    `name`         varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '名称|菜单或按钮',
-    `page`         varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '页面|路由',
-    `request`      varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin          DEFAULT NULL COMMENT '请求|接口',
-    `parent`       char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin               DEFAULT NULL COMMENT '父id',
     `created_by`   varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '创建人',
     `created_time` datetime                                               NOT NULL COMMENT '创建时间',
     `update_by`    varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '更新人',
     `update_time`  datetime                                                        DEFAULT NULL COMMENT '更新时间',
     `dele`         varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin            DEFAULT NULL COMMENT '删除',
+    `name`         varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '名称|菜单或按钮',
+    `page`         varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '页面|路由',
+    `request`      varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin          DEFAULT NULL COMMENT '请求|接口',
+    `parent`       char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin               DEFAULT NULL COMMENT '父id',
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='资源';
 
@@ -105,14 +105,14 @@ DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role`
 (
     `id`           varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL DEFAULT '' COMMENT 'id',
-    `name`         varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '角色',
-    `desc`         varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '描述',
-    `company_id`   varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '公司自定义角色',
     `created_by`   varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '创建人',
     `created_time` datetime                                               NOT NULL COMMENT '创建时间',
     `update_by`    varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '更新人',
     `update_time`  datetime                                                        DEFAULT NULL COMMENT '更新时间',
     `dele`         varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin            DEFAULT NULL COMMENT '删除',
+    `name`         varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '角色',
+    `desc`         varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '描述',
+    `company_id`   varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  NOT NULL COMMENT '公司自定义角色',
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='角色';
 
@@ -129,13 +129,13 @@ DROP TABLE IF EXISTS `user_role_resource`;
 CREATE TABLE `user_role_resource`
 (
     `id`           varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT 'id',
-    `role_id`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '角色|id',
-    `resource_id`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '资源|id',
     `created_by`   varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '创建人',
     `created_time` datetime                                              NOT NULL COMMENT '创建时间',
     `update_by`    varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin          DEFAULT NULL COMMENT '更新人',
     `update_time`  datetime                                                       DEFAULT NULL COMMENT '更新时间',
     `dele`         varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '删除',
+    `role_id`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '角色|id',
+    `resource_id`  varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '资源|id',
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='角色资源关联';
 
@@ -152,13 +152,13 @@ DROP TABLE IF EXISTS `user_role_user`;
 CREATE TABLE `user_role_user`
 (
     `id`           varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT 'id',
-    `role_id`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '角色|id',
-    `user_id`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '用户|id',
     `created_by`   varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '创建人',
     `created_time` datetime                                              NOT NULL COMMENT '创建时间',
     `update_by`    varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin          DEFAULT NULL COMMENT '更新人',
     `update_time`  datetime                                                       DEFAULT NULL COMMENT '更新时间',
     `dele`         varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin           DEFAULT NULL COMMENT '删除',
+    `role_id`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '角色|id',
+    `user_id`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '用户|id',
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='角色用户关联';
 
